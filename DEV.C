dc0d6e4c2efc9584ff9c4cf8194cc0694a6d3a9a
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-30 16:16:35 +0530

    Create DEV.C

---
+++ DEV.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[20];
+int i,n,k,sum=0;
+clrscr();
+printf("enter the size of array");
+scanf("%d",&n);
+printf("enter k values of sum");
+scanf("%d",&k);
+printf("enter elements of array",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a(i));
+}
+for(i=o;i<k;i++)
+{
+sum=sum+a(i);
+}
+printf("sum of k values are %d",sum);
+}
+getch();
+}
